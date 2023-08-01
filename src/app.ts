0625557c976c8077492d2416af4c50cf16d1c629
erhanask <erhanasik8@gmail.com>
2023-08-01 20:59:01 +0300

    added validation for inputs

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,35 @@
+// Validation Interface
+interface Validatable {
+    value: string | number;
+    required?: boolean;
+    minLength?: number;
+    maxLength?: number;
+    min?: number;
+    max?: number;
+}
+
+function validate(input: Validatable) {
+    let isValid = true;
+    if (input.required) {
+        isValid = isValid && input.value.toString().trim().length !== 0;
+    }
+    if (input.minLength != null && typeof input.value === 'string') {
+        isValid = isValid && input.value.length >= input.minLength;
+    }
+    if (input.maxLength != null && typeof input.value === 'string') {
+        isValid = isValid && input.value.length <= input.maxLength;
+    }
+    if (input.min != null && typeof input.value === 'number') {
+        isValid = isValid && input.value >= input.min;
+    }
+    if (input.max != null && typeof input.value === 'number') {
+        isValid = isValid && input.value <= input.max;
+    }
+    console.log(isValid);
+    return isValid;
+}
+
+
 // autobind decorator
 function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
     const originalMethod = descriptor.value;
@@ -30,10 +62,45 @@ class ProjectInput {
         this.attach();
     }
 
+    private gatherUserInput(): [string, string, number] | void {
+        const title = this.titleInputElement.value;
+        const description = this.descriptionInputElement.value;
+        const people = this.peopleInputElement.value;
+
+        const titleValidatable: Validatable = {
+            value: title,
+            required: true
+        };
+        const descriptionValidatable: Validatable = {
+            value: description,
+            required: true,
+            minLength: 5,
+            maxLength: 100
+        };
+        const peopleValidatable: Validatable = {
+            value: +people,
+            required: true,
+            min: 1,
+            max: 5
+        }
+
+        if (
+            !validate(titleValidatable) ||
+            !validate(descriptionValidatable) ||
+            !validate(peopleValidatable)
+        ) {
+            alert('Invalid input, please try again!');
+            return;
+        } else {
+            return [title, description, +people];
+        }
+
+    }
+
     @autobind
     private submitHandler(event: Event) {
         event.preventDefault();
-        console.log(this.peopleInputElement.value);
+        console.log(this.gatherUserInput());
     }
 
     private configure() {
